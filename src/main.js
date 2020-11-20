6776ea41abf5d28a5054e8c0a489d17eb326e72d
Nataliya Zhuravleva <natalindria@gmail.com>
2020-11-20 11:19:35 -1000

    Check response does't include currency

--- src/main.js
+++ src/main.js
@@ -5,32 +5,37 @@ import '../src/css/styles.css';
 import CurrencyService from '../src/js/exchanger';
 
 function convert(amount, currency) {
-  return parseFloat(amount) * parseFloat(currency);
+  return parseFloat(amount) * parseFloat(currency).toFixed(2);
 }
 
 function clearFields() {
   $('#amount').val("");
-  $('#currency').val("EUR");
+  $('#currency').val("noChoosen");
   $('.showConvertedAmount').text("");
+  $('.showCurrencyError').text("");
   $('.showErrors').text("");
 }
 
 function getElements(amount, currency, response) {
 
   if (response.conversion_rates) {
-    let result=0;
-    if (currency === "EUR") {
-      result = convert(amount, response.conversion_rates.EUR);
-    } else if (currency === "RUB") {
-      result = convert(amount, response.conversion_rates.RUB);
-    } else if (currency === "CAD") {
-      result = convert(amount, response.conversion_rates.CAD);
-    } else if (currency === "MXN") {
-      result = convert(amount, response.conversion_rates.MXN);
-    } else if (currency === "SEK") {
-      result = convert(amount, response.conversion_rates.SEK);
+    if (currency in response.conversion_rates) {
+      let result = 0;
+      if (currency === "EUR") {
+        result = convert(amount, response.conversion_rates.EUR);
+      } else if (currency === "RUB") {
+        result = convert(amount, response.conversion_rates.RUB);
+      } else if (currency === "CAD") {
+        result = convert(amount, response.conversion_rates.CAD);
+      } else if (currency === "MXN") {
+        result = convert(amount, response.conversion_rates.MXN);
+      } else if (currency === "SEK") {
+        result = convert(amount, response.conversion_rates.SEK);
+      }
+      $('.showConvertedAmount').text(`${amount} USD in ${currency} is ${result}`);
+    } else {
+      $('.showCurrencyError').text(`We don't have an information about ${currency}`);
     }
-    $('.showConvertedAmount').text(`${amount} USD in ${currency} is ${result}`);
   } else {
     $('.showErrors').text(`There was an error: ${response}`);
   }
@@ -40,7 +45,8 @@ function getElements(amount, currency, response) {
 $(document).ready(function () {
   $('#convert').click(function () {
     let amount = $('#amount').val();
-    let currencyType = $('#currency').val();
+    let currencyType = "ABC";
+    //let currencyType = $('#currency').val();
     clearFields();
 
 
