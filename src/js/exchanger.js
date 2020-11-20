7419bbd45c5087a155e29831361e529f6be2fbe2
Nataliya Zhuravleva <natalindria@gmail.com>
2020-11-20 08:49:28 -1000

    Add getElements with errors handling

--- src/js/exchanger.js
+++ src/js/exchanger.js
@@ -1,5 +1,5 @@
 export default class CurrencyService {
-  static async getCurrency(currency) {
+  static async getCurrency() {
     try {
       const currencyResponse = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`);
       if (!currencyResponse.ok) {
--- src/main.js
+++ src/main.js
@@ -4,10 +4,23 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import '.css/styles.css';
 import CurrencyService from '.js/exchanger';
 
+function getElements(response){
+  if (response.conversion_rates) {
+    //convert function
+  } else {
+    $('showErrors').text(`There was an error: ${response}`);
+  }
+}
+
 $(document).ready(function(){
   $('#convert').click(function(){
     let amount = $('#amount').val();
     let currencyType = $('#currency').val();
-    CurrencyService.getCurrency();
-  })
-})
+    (async function(){
+      const currencyResponse = await CurrencyService.getCurrency(currencyType);
+      getElements(currencyResponse);
+    });
+   
+
+  });
+});
