033f6b3a8c1d6822b45c5d21f23df07f24e97ab6
Nataliya Zhuravleva <natalindria@gmail.com>
2020-11-20 07:10:31 -1000

    Add try catch with error.message return

--- src/js/exchanger.js
+++ src/js/exchanger.js
@@ -1,5 +1,10 @@
 export default class CurrencyService {
   static async getCurrency(currency) {
-  const currencyResponse = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`);
-  return currencyResponse.json();
+    try {
+      const currencyResponse = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`);
+      return currencyResponse.json();
+    } catch (error) {
+      return error.message;
+    }
+  }
 }
